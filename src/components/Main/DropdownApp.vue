f443fbbcce45bda0f300832c52d46da3d97200ba
LeoSallu <L.sallustio99@gmail.com>
2023-03-28 11:39:31 +0200

    menu dropdown dinamico

--- src/components/Main/DropdownApp.vue
+++ src/components/Main/DropdownApp.vue
@@ -1,17 +1,20 @@
 <script>
+import { store } from '../../store';
 export default{
-    
+    data(){
+        return{
+            store
+        }
+    }
 }
 </script>
 <template>
     <div class="dropdown-center my-3">
             <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                Alien
+               Scegli la tua carta
             </button>
             <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Action two</a></li>
-                <li><a class="dropdown-item" href="#">Action three</a></li>
+                <li v-for="card in (store.cards).slice(20,40)"><a class="dropdown-item" href="#">{{card.name}}</a></li>
             </ul>
         </div>
 </template>
